e8132547a1bcd1514619328e9fbcc2a2c203e298
Matthew Buchanan <matt@Matthews-MacBook-Air.local>
2018-05-26 09:47:42 -0400

    JAWSdb for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 //var keys = require('../public/js/keys.js');
 require('dotenv').config();
 var mysql = require("mysql");
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+connection = mysql.createConnection({
   host: process.env.SQL_HOST,
   port: process.env.SQL_PORT,
   user: process.env.SQL_USER,
   password: process.env.SQL_PASSWORD,
   database: process.env.SQL_DATABASE
 });
+}
 
 
 connection.connect(function(err) {
